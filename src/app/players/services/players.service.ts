bcb811e1c9163e96f5979346397b0e1b0447199b
Alex Greco <agreco3@gatech.edu>
2018-02-21 20:03:29 -0600

    Just painstakingly created css color schemes for all the team tables.

--- src/app/players/services/players.service.ts
+++ src/app/players/services/players.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 import * as _ from 'lodash';
 import 'rxjs/add/operator/map';
 
