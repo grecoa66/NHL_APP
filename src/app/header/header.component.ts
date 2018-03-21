e5577e397961316b07a33f804b2059afb729a7ea
grecoa66 <akgrecks88@gmail.com>
2018-03-21 18:56:45 -0400

    Fixed the bootstrap navbar collapse issue.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+    public isCollapsed = true;
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
