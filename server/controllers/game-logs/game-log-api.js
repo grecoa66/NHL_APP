aa0dfa5d50e22f6af2e3d9f4fea67a55f845c81a
grecoa66 <akgrecks88@gmail.com>
2018-05-14 21:36:17 -0400

    Added some more content to the home page.

---
+++ server/controllers/game-logs/game-log-api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const urlList = require('../../common/url-list');
+
+router.get('/:year', (req, res) => {
+
+
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const teams = require('../controllers/team/team-api');
 const players = require('../controllers/player/player-api');
 const playoffs = require('../controllers/playoffs/playoffs-api');
+const gamelog = require('../controllers/game-logs/game-log-api');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -14,5 +15,7 @@ router.get('/', (req, res) => {
 router.use('/players', players);
 
 router.use('/teams', teams);
+
+router.use('gamelog', gamelog);
 
 module.exports = router;
